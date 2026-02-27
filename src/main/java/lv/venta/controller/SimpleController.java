11f2ecaa462afa9dc6d1022d7883d44d87912e97
MikuChanz <mikusvalts@gmail.com>
2026-02-27 18:07:23 +0200

    Edit: Added all of the files required

---
+++ src/main/java/lv/venta/controller/SimpleController.java
@@ -0,0 +1,84 @@
+package lv.venta.controller;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import lv.venta.model.Category;
+import lv.venta.model.Product;
+//kontrolieru klase ar endpointiem
+@Controller
+public class SimpleController {
+	
+	
+	ArrayList<Product> allProducts = new ArrayList<>(Arrays.asList(
+			new Product("Ābols", 0.99f, "Salds un garšīgs", Category.fruit, 10),
+			new Product("Burkāns", 2.84f, "Oranžš", Category.vegetable, 40),
+			new Product("Vīnogas", 4.99f, "Violetas", Category.fruit, 3)
+			)
+			);
+	
+	
+	@GetMapping("/showmessage") //localhost:8080/showmessage
+	public String getShowMessage() {
+		System.out.println("Mans pirmais kontrolieris ir izsaukts");
+		return "msg";//šī koda rinda parādāsi msg.html lapu
+		
+	}
+	
+	@GetMapping("/showdata")//localhost:8080/showdata
+	public String getShowData(Model model) {
+		System.out.println("Otrais kontrolieris ir izsaukts");
+		Random rand = new Random();
+		String text = "Karina " + rand.nextInt();
+		model.addAttribute("package", text);
+		return "data";//sī koda rinda parādīs data.html lapu
+	}
+	
+	
+	@GetMapping("/getproduct")//localhost:8080/getproduct
+	private String getGetProduct(Model model) {
+		Product prod = new Product("Ābols", 0.99f, "Salds un garšīgs", Category.fruit, 10);
+		model.addAttribute("package", prod);
+		return "show-one-product";//sī koda rinda parādīs show-one-product.html lapu
+		
+	}
+	
+	
+	@GetMapping("/getallproducts")//localhost:8080/getallproducts
+	public String getGetAllProducts(Model model) {
+		model.addAttribute("package",allProducts);
+		return "show-all-products";//sī koda rinda parādīs show-all-products.html lapu
+	}
+	
+	@GetMapping("/getallproducts/{id}")//localhost:8080/getallproducts/1
+	public String getGetAllProductsById(@PathVariable(name = "id") int id, Model model) {
+		if(id < 0) {
+			model.addAttribute("package", "Produkta id nevar būt negatīvs");
+			return "error-page";//šī koda rinda parādīs error-page.html lapu ar ziņū, ka id nevar būt negatīvs
+		}
+		
+		for(Product tempP : allProducts) {
+			if(tempP.getId()==id) {
+				model.addAttribute("package", tempP);
+				return "show-one-product";//sī koda rinda parādīs show-one-product.html lapu
+			}
+		}
+		
+		model.addAttribute("package", "Produkts ar tādu id neeksistē");
+		return "error-page";
+		
+		
+	}
+	
+	
+	//localhost:8080/allproducts?id=1
+	
+	
+
+}
---
+++ src/main/java/lv/venta/model/Product.java
@@ -0,0 +1,86 @@
+package lv.venta.model;
+
+public class Product {
+	private int id;
+	private float price;
+	private String title;
+	private Category category;
+	private String description;
+	private int quantity;
+	
+	private static int counter = 0;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId() {
+		this.id = counter;
+		counter++;
+	}
+	public float getPrice() {
+		return price;
+	}
+	public void setPrice(float price) {
+		this.price = price;
+	}
+	public String getTitle() {
+		return title;
+	}
+	
+	//TODO Izmantot validāciju anotācijas
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public Category getCategory() {
+		return category;
+	}
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	
+	public Product()
+	{
+		setId();
+		setTitle("Galds");
+		setPrice(78.99f);
+		setDescription("Brūns un liels");
+		setCategory(Category.other);
+		setQuantity(4);
+	}
+	
+	public Product(String inputTitle, float inputPrice, String inputDescription, Category inputCategory,
+			int inputQuantity) {
+		setId();
+		setTitle(inputTitle);
+		setPrice(inputPrice);
+		setDescription(inputDescription);
+		setCategory(inputCategory);
+		setQuantity(inputQuantity);
+	}
+	
+	public String toString() {
+		return id + ": " + title + ", " + price + " EUR, " + description + ", " + category + ", " + quantity; 
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
